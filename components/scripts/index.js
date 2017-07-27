4a1ec64e8f1e0755adea27584dd920c61123cf85
Azad Asanali. Abbasali <aabbasal@oradev.oraclecorp.com>
2017-07-27 23:39:25 +0530

    Included compass and browserify to gulp tasks

--- components/scripts/index.js
+++ components/scripts/index.js
@@ -1,1 +1,6 @@
-console.log('Index JS Loaded');
+var $ = require('jquery');
+
+$(function() {    
+    console.log('JQuery Loaded');
+    $('#welcome').text('The DOM is now loaded and can be manipulated.');
+});
--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,32 @@
 var gulp = require('gulp'),
     gutil = require('gulp-util'),
+    browserify = require('browserify'),
+    source = require('vinyl-source-stream'),
+    compass = require('gulp-compass'),
     concat = require('gulp-concat');
 
 var jsSources = [
     'components/scripts/index.js'
 ];
 
+var sassSources = [ 'components/sass/style.scss' ];
+
 gulp.task('js', function() {
-    gulp.src(jsSources)
-    .pipe(concat('script.js'))
-    .pipe(gulp.dest('builds/development/js'))
+    var bundleStream = browserify(jsSources).bundle();
+
+    bundleStream
+        .pipe(source('script.js'))
+        .pipe(gulp.dest('builds/development/js'))
+});
+
+
+gulp.task('compass', function() {
+    gulp.src(sassSources)
+    .pipe(compass({
+        sass: 'components/sass',
+        image: 'builds/development/images',
+        style: 'expanded'
+    })
+    .on('error', gutil.log))
+    .pipe(gulp.dest('builds/development/css'))
 });
