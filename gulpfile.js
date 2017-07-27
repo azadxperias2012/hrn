11e35c3dec9c26a63d1599fb0357042df26a3085
Azad Asanali. Abbasali <aabbasal@oradev.oraclecorp.com>
2017-07-27 20:54:20 +0530

    Concatenated JavaScript files

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,13 @@
 var gulp = require('gulp'),
-    gutil = require('gulp-util');
+    gutil = require('gulp-util'),
+    concat = require('gulp-concat');
 
-gulp.task('log', function() {
-    gutil.log('Logs a message');
+var jsSources = [
+    'components/scripts/index.js'
+];
+
+gulp.task('js', function() {
+    gulp.src(jsSources)
+    .pipe(concat('script.js'))
+    .pipe(gulp.dest('builds/development/js'))
 });
